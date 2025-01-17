23127bbba50c195ed979d87aede6e708ca5e3b3f
Burak Mehmet Gürbüz <burak@burakgurbuz.com>
2025-01-17 21:39:15 +0300

    App scaffold commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <ToolBar />
+
+    <AppCanvas />
+</template>
+
+<script setup lang="ts">
+import AppCanvas from './components/AppCanvas.vue';
+import ToolBar from './components/ToolBar.vue';
+</script>
---
+++ src/components/ToolBar.vue
@@ -0,0 +1,13 @@
+<template>
+    <aside>
+        <nav>
+            
+        </nav>
+    </aside>
+</template>
+
+<script lang="ts">
+export default {
+    
+}
+</script>
